4b4e3557199c993f5603db2a96477357069b688d
Gabriel Divtech <gabriel.lira@divtech.com.br>
2023-06-14 00:03:30 -0300

    Alteração do debug

--- projeto_django_news/settings.py
+++ projeto_django_news/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yp*!dqv&to*lxr4e$(00dxvg&k$!fc$54vrt_1kckc58n=domj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
