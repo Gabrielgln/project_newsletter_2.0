66df8dac5a110bbbd011411b81e8d83fbe25c1c2
Gabriel Divtech <gabriel.lira@divtech.com.br>
2023-06-16 16:37:28 -0300

    Inclusão do type=text/css

--- projeto_django_news/settings.py
+++ projeto_django_news/settings.py
@@ -138,22 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MIME_TYPE_CSS = 'text/css'
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-
-STATICFILES_MIME_TYPES = {
-    'text/css': [MIME_TYPE_CSS],
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
